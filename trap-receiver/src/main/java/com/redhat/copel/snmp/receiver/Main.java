e002abc71f7ad92816c1f1eae6c51f51cb44aff6
vinicius-martinez <vinicius.b.martinez@gmail.com>
2018-10-26 13:19:25 -0300

    fixing classpath issues

---
+++ trap-receiver/src/main/java/com/redhat/copel/snmp/receiver/Main.java
@@ -0,0 +1,17 @@
+package com.redhat.copel.snmp.receiver;
+
+import org.snmp4j.smi.UdpAddress;
+import com.redhat.copel.snmp.receiver.TrapReceiver;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TrapReceiver trapReceiver = new TrapReceiver();
+		try {
+			trapReceiver.listen(new UdpAddress("0.0.0.0/162"));
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
